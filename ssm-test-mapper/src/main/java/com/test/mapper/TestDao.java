23d3ae2d8721ef111c3b5b581ea612232bd61326
贪狼 <7686212@qq.com>
2018-06-02 16:56:19 +0800

    修复一个bug

--- ssm-test-mapper/src/main/java/com/test/mapper/TestDao.java
+++ ssm-test-mapper/src/main/java/com/test/mapper/TestDao.java
@@ -16,7 +16,7 @@ public interface TestDao {
     @Insert("insert into tb_item (created,updated,title,price,num) values (now(),now(),#{tbItem.title},#{tbItem.price},#{tbItem.num})")
     Boolean addItem(@Param("tbItem") TbItem tbItem);
 
-    @Update("update tb_item set updated=now(),title =#{tbItem.title},price=#{tbItem.price},num=#{tbItem.num}) where id = #{tbItem.id}")
+    @Update("update tb_item set updated=now(),title =#{tbItem.title},price=#{tbItem.price},num=#{tbItem.num} where id = #{tbItem.id}")
     Boolean updateItem(@Param("tbItem") TbItem tbItem);
 
     @Delete("delete from tb_item where id=#{id}")
--- ssm-test-pojo/src/main/java/com/test/pojo/TbItem.java
+++ ssm-test-pojo/src/main/java/com/test/pojo/TbItem.java
@@ -8,19 +8,12 @@ public class TbItem  extends BasicDTO {
 
     private String title;
 
-    private String sellPoint;
-
     private Long price;
 
     private Integer num;
 
-    private String barcode;
 
-    private String image;
 
-    private Long cid;
-
-    private Byte status;
 
 
 }
